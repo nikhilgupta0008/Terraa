b05710d38733500f8dfdd841b5d280f19829cfb5
nikhilgupta0008 <nikhilgupta0596@gmail.com>
2022-06-29 15:21:02 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "access_key" {
+  default = "AAAAAAAAAAAAAAAAAAAAAAAA"
+}
+
+variable "secret_key" {
+  default = "AAAAAAAAAAAAAAAAAAAAAAAA"
+}
+
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "az1" {
+  default = "us-east-1a"
+}
+
+variable "az2" {
+  default = "us-east-1b"
+}
+
+variable "scenario" {
+  default = "test-tgw"
+}
+
+variable "public_key" {
+  default = "ssh-rsa aksdjkhndsliokdsfopl'mdskJpn"
+}
